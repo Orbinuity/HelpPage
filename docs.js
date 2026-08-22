0421eb62bd7d4d1d21b1eb405886b048373e2236
Flurop <flora@ratgers.nl>
2026-08-23 00:14:51 +0200

    New doc generator

---
+++ docs.js
@@ -0,0 +1,18 @@
+fetch('https://orbinuity.nl/nogo/projects.json')
+    .then(res => res.json())
+    .then(projects => {
+        const cardList = document.getElementById('project-list');
+        Object.keys(projects).forEach(project => {
+            const a = document.createElement('a');
+            a.className = "no-underline"
+            a.href = `/doc?project=${project}`
+
+            const button = document.createElement('button');
+            button.textContent = projects[project][0];
+
+            a.appendChild(button)
+            cardList.appendChild(a);
+
+            cardList.appendChild(document.createTextNode(' '));
+        });
+    });
